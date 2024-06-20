9726bfa84f98adaad2879ca32341eb884ccc1730
Ugo <ugommirikwe@gmail.com>
2024-06-20 13:15:57 +0200

    Implement Functionality for Structs

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CopyableMacro",
-            targets: ["CopyableMacro"]
+            targets: ["CopyableMacroMacros"]
         ),
         .executable(
             name: "CopyableMacroClient",
--- Tests/CopyableMacroTests/CopyableMacroTests.swift
+++ Tests/CopyableMacroTests/CopyableMacroTests.swift
@@ -7,40 +7,257 @@ import XCTest
 import CopyableMacroMacros
 
 let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
+    "Copyable": CopyableMacroMacro.self,
 ]
 #endif
 
 final class CopyableMacroTests: XCTestCase {
-    func testMacro() throws {
-        #if canImport(CopyableMacroMacros)
+//    func testMacro() throws {
+//        #if canImport(CopyableMacroMacros)
+//        assertMacroExpansion(
+//            """
+//            #stringify(a + b)
+//            """,
+//            expandedSource: """
+//            (a + b, "a + b")
+//            """,
+//            macros: testMacros
+//        )
+//        #else
+//        throw XCTSkip("macros are only supported when running tests for the host platform")
+//        #endif
+//    }
+    
+    func testMacroForClass() {
         assertMacroExpansion(
             """
-            #stringify(a + b)
+            @Copyable
+            class Sample {
+                var x: Int
+                let y: Double
+            
+                var myComputedProperty: String {
+                    "hello world"
+                }
+            
+                private var _something: Bool
+            
+                var something: Bool {
+                    get {
+                        return _something
+                    }
+                    set {
+                        _something = newValue
+                    }
+                }
+            
+                func sayHi() {
+            
+                }
+            
+                func sayBye() { }
+            }
             """,
-            expandedSource: """
-            (a + b, "a + b")
+            expandedSource:
+            """
+            
+            class Sample {
+                var x: Int
+                let y: Double
+            
+                var myComputedProperty: String {
+                    "hello world"
+                }
+            
+                private var _something: Bool
+            
+                var something: Bool {
+                    get {
+                        return _something
+                    }
+                    set {
+                        _something = newValue
+                    }
+                }
+            
+                func sayHi() {
+            
+                }
+            
+                func sayBye() { }
+            }
             """,
             macros: testMacros
         )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
     }
-
-    func testMacroWithStringLiteral() throws {
-        #if canImport(CopyableMacroMacros)
+    
+    func testMacroForStruct() {
         assertMacroExpansion(
-            #"""
-            #stringify("Hello, \(name)")
-            """#,
-            expandedSource: #"""
-            ("Hello, \(name)", #""Hello, \(name)""#)
-            """#,
+            """
+            struct User {
+                let name: String
+                let followers: [String]
+            }
+            
+            @Copyable
+            struct Sample {
+                var x: Int
+                let y: Double
+                var g: String? = nil
+                let users: [User]
+            
+                var myComputedProperty: String {
+                    "hello world"
+                }
+            
+                private var _something: Bool
+            
+                var something: Bool {
+                    get {
+                        return _something
+                    }
+                    set {
+                        _something = newValue
+                    }
+                }
+            
+                func sayHi() {
+            
+                }
+            
+                func sayBye() { }
+            }
+            """,
+            expandedSource:
+            """
+            struct User {
+                let name: String
+                let followers: [String]
+            }
+            struct Sample {
+                var x: Int
+                let y: Double
+                var g: String? = nil
+                let users: [User]
+            
+                var myComputedProperty: String {
+                    "hello world"
+                }
+            
+                private var _something: Bool
+            
+                var something: Bool {
+                    get {
+                        return _something
+                    }
+                    set {
+                        _something = newValue
+                    }
+                }
+            
+                func sayHi() {
+            
+                }
+            
+                func sayBye() { }
+            
+                public func copy(
+                    x: Int? = nil,
+                    y: Double? = nil,
+                    g: String? = nil,
+                    users: [User]? = nil
+                ) -> Self {
+                    .init(
+                        x: x ?? self.x,
+                        y: y ?? self.y,
+                        g: g ?? self.g,
+                        users: users ?? self.users
+                    )
+                }
+            }
+            """,
             macros: testMacros
         )
-        #else
-        throw XCTSkip("macros are only supported when running tests for the host platform")
-        #endif
+    }
+    
+    func testLazyPropsAreTreatedProperly() {
+        assertMacroExpansion(
+            """
+            @Copyable
+            class Sample {
+                lazy var x: Int = 1
+                let y: Double
+            }
+            """,
+            expandedSource:
+            """
+            
+            class Sample {
+                lazy var x: Int = 1
+                let y: Double
+            }
+            """,
+            macros: testMacros
+        )
+    }
+    
+    func testLetPropertiesWithValuesAreTreatedCorrectly() {
+        assertMacroExpansion(
+            """
+            @Copyable
+            class Sample {
+                let y: Double = 0
+            }
+            """,
+            expandedSource:
+            """
+            
+            class Sample {
+                let y: Double = 0
+            }
+            """,
+            macros: testMacros
+        )
+    }
+    
+    // FIXME: this should treated as a separate edge case
+    func testDuplicatedInitsAreNotCreated() {
+        assertMacroExpansion(
+            """
+            @Copyable
+            struct Sample {
+                public init() {
+            
+                }
+            }
+            """,
+            expandedSource:
+            """
+            
+            struct Sample {
+                public init() {
+            
+                }
+            }
+            """,
+            macros: testMacros
+        )
+    }
+    
+    // FIXME: this should treated as a separate edge case
+    func testInitsAreNotCreated() {
+        assertMacroExpansion(
+            """
+            @Copyable
+            struct Sample {
+            }
+            """,
+            expandedSource:
+            """
+            struct Sample {
+            }
+            """,
+            macros: testMacros
+        )
     }
 }
